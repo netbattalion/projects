dcd4106e917ffb8da0bff8d9d252cd4766d85e57
netbattalion <hempel@netbattalion.de>
2024-10-11 02:39:09 +0200

    Update netbattalionJS.js
    Strict Mode

--- netbatJS/netbattalionJS.js
+++ netbatJS/netbattalionJS.js
@@ -1,23 +1,27 @@
-; ( ( ) => {
+"use strict";
+
+let ref, me
+ ( ( ) => {
     let r = {}, f = {}
-    for( i of document .querySelectorAll( "[data-bind]" ) ) {
+    for( let i of document .querySelectorAll( "[data-bind]" ) ) {
         let d = i .dataset,
-// ---- Variables ----------------------------------------
         b = d .bind .split( "=" ),
-        y = b[ 0 ] .trim()
-        b[ 1 ] && ( r[ y ] = eval( b[ 1 ] ) )
-
-// ---- Functions ----------------------------------------
-        w = f[ y ] ?? ( f[ y ] = [] ),
+            
+// ---- Variables and Functions ---------------------------------
+            
+        y = b[ 0 ] .trim(),
+        w = f[ y ] ?? ( f[ y ] = [] )
         w .push( [ i, Function( d .set ) ] )
+        b[ 1 ] && ( r[ y ] = eval( b[ 1 ] ) )
     }
-
-// ---- Observe ------------------------------------------
+     
+// ---- Bind Handlers -------------------------------------------
+     
     ref = new Proxy(
         r, {
             set: ( r, p, v ) => {
                 r[ p ] = v
-                for ( i of f[ p ] ) i[ 1 ] ( me = i[ 0 ] ) 
+                for ( let i of f[ p ] ) i[ 1 ] ( me = i[ 0 ] )
             }
         }
     )
